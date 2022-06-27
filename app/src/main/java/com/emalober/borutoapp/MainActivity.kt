0befe2f6ac921f8553cffeab6108bf6f90662629
emaloberti <jemaloberti@falabella.com.ar>
2022-06-27 14:37:03 -0300

    Setup navigation

--- app/src/main/java/com/emalober/borutoapp/MainActivity.kt
+++ app/src/main/java/com/emalober/borutoapp/MainActivity.kt
@@ -10,34 +10,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.emalober.borutoapp.navigation.SetupNavGraph
 import com.emalober.borutoapp.ui.theme.BorutoAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BorutoAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BorutoAppTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/emalober/borutoapp/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.emalober.borutoapp.util
+
+object Constants {
+
+    const val DETAILS_ARGUMENT_KEY = "heroId"
+}
