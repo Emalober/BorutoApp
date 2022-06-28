bd2e4fa5d508991fe70ec492c72ce7b5d35b2fa3
emaloberti <jemaloberti@falabella.com.ar>
2022-06-28 09:37:21 -0300

    Setup Dagger-Hilt

--- app/src/main/java/com/emalober/borutoapp/MainActivity.kt
+++ app/src/main/java/com/emalober/borutoapp/MainActivity.kt
@@ -14,7 +14,9 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.emalober.borutoapp.navigation.SetupNavGraph
 import com.emalober.borutoapp.ui.theme.BorutoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private lateinit var navController: NavHostController
