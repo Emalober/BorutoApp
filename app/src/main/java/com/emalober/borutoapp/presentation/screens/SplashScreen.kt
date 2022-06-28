0376e0bc2ce13cabbc76e4741a0ae5f0c84a3e5f
emaloberti <jemaloberti@falabella.com.ar>
2022-06-28 16:19:00 -0300

    Create animation in logo's splash

--- app/src/main/java/com/emalober/borutoapp/presentation/screens/SplashScreen.kt
+++ app/src/main/java/com/emalober/borutoapp/presentation/screens/SplashScreen.kt
@@ -1,14 +1,19 @@
 package com.emalober.borutoapp.presentation.screens
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -26,6 +31,18 @@ fun SplashScreen() {
 
 @Composable
 fun Splash() {
+
+    val degrees = remember { Animatable(0f) }
+    LaunchedEffect(key1 = true) {
+        degrees.animateTo(
+            targetValue = 360f,
+            animationSpec = tween(
+                durationMillis = 1000,
+                delayMillis = 200
+            )
+        )
+    }
+
     Box(
         modifier = Modifier
             .background(
@@ -39,7 +56,8 @@ fun Splash() {
         contentAlignment = Alignment.Center
     ) {
         Image(
-            painterResource(id = R.drawable.ic_logo),
+            modifier = Modifier.rotate(degrees = degrees.value),
+            painter = painterResource(id = R.drawable.ic_logo),
             contentDescription = stringResource(id = R.string.app_logo)
         )
     }
