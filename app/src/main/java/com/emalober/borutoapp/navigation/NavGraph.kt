f546d4d026afcd2ba496c0003808f652bae31037
emaloberti <jemaloberti@falabella.com.ar>
2022-06-29 15:53:41 -0300

    Create nbarding

--- app/src/main/java/com/emalober/borutoapp/navigation/NavGraph.kt
+++ app/src/main/java/com/emalober/borutoapp/navigation/NavGraph.kt
@@ -7,27 +7,42 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.emalober.borutoapp.presentation.screens.SplashScreen
+import com.emalober.borutoapp.presentation.screens.WelcomeScreen
 import com.emalober.borutoapp.util.Constants
 
 @Composable
 fun SetupNavGraph(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = Screen.Splash.route) {
+        startDestination = Screen.Splash.route
+    ) {
 
         composable(route = Screen.Splash.route) {
-            SplashScreen()
+            SplashScreen(
+                goToHome = {
+                    navController.popBackStack()
+                    navController.navigate(Screen.Home.route)
+                },
+                goToWelcome = {
+                    navController.popBackStack()
+                    navController.navigate(Screen.Welcome.route)
+                }
+            )
         }
         composable(route = Screen.Welcome.route) {
-
+            WelcomeScreen(goToHome = {
+                navController.popBackStack()
+                navController.navigate(Screen.Home.route)
+            })
         }
         composable(route = Screen.Home.route) {
 
         }
         composable(route = Screen.Details.route,
-        arguments = listOf(navArgument(Constants.DETAILS_ARGUMENT_KEY) {
-            type = NavType.IntType
-        })) {
+            arguments = listOf(navArgument(Constants.DETAILS_ARGUMENT_KEY) {
+                type = NavType.IntType
+            })
+        ) {
 
         }
         composable(route = Screen.Search.route) {
--- app/src/main/java/com/emalober/borutoapp/presentation/screens/SplashScreen.kt
+++ app/src/main/java/com/emalober/borutoapp/presentation/screens/SplashScreen.kt
@@ -2,15 +2,14 @@ package com.emalober.borutoapp.presentation.screens
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -20,19 +19,20 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.emalober.borutoapp.R
+import com.emalober.borutoapp.navigation.Screen
 import com.emalober.borutoapp.ui.theme.BorutoAppTheme
 import com.emalober.borutoapp.ui.theme.Purple500
 import com.emalober.borutoapp.ui.theme.Purple700
+import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen() {
-    Splash()
-}
-
-@Composable
-fun Splash() {
-
+fun SplashScreen(
+    timeMillis: Long = 4000,
+    goToHome: () -> Unit,
+    goToWelcome: () -> Unit
+) {
     val degrees = remember { Animatable(0f) }
+
     LaunchedEffect(key1 = true) {
         degrees.animateTo(
             targetValue = 360f,
@@ -41,7 +41,17 @@ fun Splash() {
                 delayMillis = 200
             )
         )
+        goToWelcome()
     }
+
+
+    Splash(degrees = degrees.value)
+}
+
+@Composable
+fun Splash(
+    degrees: Float = 0f
+) {
 
     Box(
         modifier = Modifier
@@ -56,7 +66,7 @@ fun Splash() {
         contentAlignment = Alignment.Center
     ) {
         Image(
-            modifier = Modifier.rotate(degrees = degrees.value),
+            modifier = Modifier.rotate(degrees = degrees),
             painter = painterResource(id = R.drawable.ic_logo),
             contentDescription = stringResource(id = R.string.app_logo)
         )
@@ -70,6 +80,7 @@ fun SplashPreview() {
         Splash()
     }
 }
+
 @Composable
 @Preview(uiMode = UI_MODE_NIGHT_YES)
 fun SplashDarkPreview() {
